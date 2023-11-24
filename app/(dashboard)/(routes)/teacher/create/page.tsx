32b3e4fe998433a18fccda280cdf112f7dc46981
Durgesh Prajapati <drgaming143@gmail.com>
2023-11-24 18:12:25 +0530

    Adding Schema to create new Course and included form from shadcn

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,38 +1,38 @@
 "use client";
 
-// import * as z from "zod";
-// import axios from "axios";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { useRouter } from "next/navigation";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
-// import {
-// 	Form,
-// 	FormControl,
-// 	FormDescription,
-// 	FormField,
-// 	FormLabel,
-// 	FormMessage,
-// } from "@/components/ui/form";
-// import { Button } from "@/components/ui/button";
-// import { Input } from "@/components/ui/input";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-// // used joi in yelp camp similarly zod is also used to create schema in typescript
-// const formSchema = z.object({
-// 	title: z.string().min(1, {
-// 		message: "Title is Required",
-// 	}),
-// });
+// used joi in yelp camp similarly zod is also used to create schema in typescript
+const formSchema = z.object({
+	title: z.string().min(1, {
+		message: "Title is Required",
+	}),
+});
 
 const CreateCoursePage = () => {
-	// const form = useForm<z.infer<typeof formSchema>>({
-	// 	resolver: zodResolver(formSchema),
-	// 	defaultValues: {
-	// 		title: "",
-	// 	},
-	// });
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			title: "",
+		},
+	});
 
-	// const { isSubmitting, isValid } = form.formState;
+	const { isSubmitting, isValid } = form.formState;
 
 	return <div className="">This Create Course Page</div>;
 };
