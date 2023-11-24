315a1431b82bea881a9132d27cf50a6976480df0
Durgesh Prajapati <drgaming143@gmail.com>
2023-11-25 00:37:13 +0530

    Add form to add title to new course and added toast for notification

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -11,11 +11,15 @@ import {
 	FormControl,
 	FormDescription,
 	FormField,
+	FormItem,
 	FormLabel,
 	FormMessage,
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import toast from "react-hot-toast"
+
 
 // used joi in yelp camp similarly zod is also used to create schema in typescript
 const formSchema = z.object({
@@ -25,6 +29,8 @@ const formSchema = z.object({
 });
 
 const CreateCoursePage = () => {
+	const router = useRouter();
+
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
@@ -34,7 +40,70 @@ const CreateCoursePage = () => {
 
 	const { isSubmitting, isValid } = form.formState;
 
-	return <div className="">This Create Course Page</div>;
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+
+		try {
+			const response = await axios.post("/api/course", values);
+			router.push(`/teacher/courses/${response.data.id}`);
+		} catch (err) {
+			if (err) {
+				toast.error("Something Went Wrong")
+			}
+		}
+	};
+
+	return (
+		<div className="max-w-5xl mx-auto flex items-center md:justify-center h-full p-6">
+			<div>
+				<h1 className="text-2xl">Name Your Course</h1>
+				<p className="text-sm text-slate-600">
+					What Would you like to name your course?
+				</p>
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-8"
+					>
+						<FormField
+							control={form.control}
+							name="title"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Course Title</FormLabel>
+									<FormControl>
+										<Input
+											disabled={isSubmitting}
+											placeholder="Advanced Web"
+											{...field}
+										/>
+									</FormControl>
+									<FormDescription>
+										What will you teach in this Course?
+									</FormDescription>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className="flex items-center gap-x-2">
+							<Link href={"/"}>
+								<Button type="button" variant="ghost">
+									Cancel
+								</Button>
+							</Link>
+
+							<Button
+								type="submit"
+								disabled={!isValid || isSubmitting}
+							>
+								Continue
+							</Button>
+						</div>
+					</form>
+				</Form>
+			</div>
+		</div>
+	);
 };
 
 export default CreateCoursePage;
