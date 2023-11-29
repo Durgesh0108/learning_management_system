3030a44a49b081cd1a5be261c7c876b354dc92c3
Durgesh Prajapati <drgaming143@gmail.com>
2023-11-29 19:12:24 +0530

    added title form page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm  from "./_components/title-form";
 import React from "react";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
@@ -40,19 +41,26 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			<div className="flex items-center justify-between">
 				<div className="flex flex-col gap-y-2">
 					<h1 className="text-2xl font-medium">Course Setup</h1>
-					<span className="text-sm text-slate-700">Complete All Fields {completionText}</span>
+					<span className="text-sm text-slate-700">
+						Complete All Fields {completionText}
+					</span>
 				</div>
-            </div>
+			</div>
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
 				<div>
 					<div className="flex items-center gap-x-2">
-						<IconBadge icon={LayoutDashboard} variant={"success"}/>
-						<h2 className="text-xl font-medium">Customize Your Course</h2>
+						<IconBadge icon={LayoutDashboard} />
+						<h2 className="text-xl font-medium">
+							Customize Your Course
+						</h2>
 					</div>
+
+					<TitleForm
+						initialData={course}
+						courseId={course.id}
+					></TitleForm>
 				</div>
-                {/* <div>{course.title}</div>
-                <div>{course.id}</div> */}
-            </div>
+			</div>
 		</div>
 	);
 };
