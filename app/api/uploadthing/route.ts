dd11a41152f7e017f09cea39ac7bcdc562355cbc
Durgesh Prajapati <drgaming143@gmail.com>
2024-01-14 13:17:52 +0530

    fixed route.ts to upload the images

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,7 +1,8 @@
-import { createServerHandler } from "uploadthing/server";
+import { createNextRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
-export const { GET, POST } = createServerHandler({
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
     router: ourFileRouter,
 });
